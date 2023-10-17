aa4bb85aadac81e37db99c1fa10fde8623dac920
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-10-17 07:01:14 -0300

    Two data binding example with TextAreas and using ngMdel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   paragraph:string = "";
   enable:boolean = false;
+  //Tow data binding
+  leftContent:string = "";
+  rightContent:string = "";
+  //ngModel
+  rContent:string = "";
+
 
   disableButton(){ this.enable = !this.enable; }
 
   copyAndPaste(){
     let textInput:any = document.getElementById("mainTextBox");
     this.paragraph = textInput.value;
+  }
+
+  reWrite(event:Event){
+    const element:HTMLTextAreaElement = event.target as HTMLTextAreaElement;
+
+    if(element.id == "leftTextArea"){
+      let r:HTMLTextAreaElement = document.getElementById("rightTextArea") as HTMLTextAreaElement;
+      r.value = element.value;
+    }
+    else if(element.id == "rightTextArea"){
+      let l:HTMLTextAreaElement = document.getElementById("leftTextArea") as HTMLTextAreaElement;
+      l.value = element.value;
+    }
   }
 }
