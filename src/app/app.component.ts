73c6470cfce3366f9fcb81bb0787bccc813e2238
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-10-20 07:35:42 -0300

    using @Input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,32 +6,9 @@ import { Component, EventEmitter } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  paragraph:string = "";
-  enable:boolean = false;
-  //Tow data binding
-  leftContent:string = "";
-  rightContent:string = "";
-  //ngModel
-  rContent:string = "";
-
-
-  disableButton(){ this.enable = !this.enable; }
-
-  copyAndPaste(){
-    let textInput:any = document.getElementById("mainTextBox");
-    this.paragraph = textInput.value;
-  }
-
-  reWrite(event:Event){
-    const element:HTMLTextAreaElement = event.target as HTMLTextAreaElement;
-
-    if(element.id == "leftTextArea"){
-      let r:HTMLTextAreaElement = document.getElementById("rightTextArea") as HTMLTextAreaElement;
-      r.value = element.value;
-    }
-    else if(element.id == "rightTextArea"){
-      let l:HTMLTextAreaElement = document.getElementById("leftTextArea") as HTMLTextAreaElement;
-      l.value = element.value;
-    }
+  myTitle?:string;
+  writeSomething(event:Event){
+    let target:HTMLTextAreaElement = event.target as HTMLTextAreaElement;
+    this.myTitle = target.value;
   }
 }
