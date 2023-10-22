0215c252b3bf821c37be2367a2a96c31ff3e0c33
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-10-21 21:32:59 -0300

    Example of a basic slider of tecnologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnInit, HostListener } from '@angular/core';
+import { Component, DoCheck, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { Location } from 'src/shared/models/Location';
 
 @Component({
@@ -6,30 +6,23 @@ import { Location } from 'src/shared/models/Location';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, DoCheck {
-  Endpoint?: string;
-  sendEndPoint?: string; //ParentInput
+export class AppComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+
+
+  //Component LifeCycle
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("appOnChange");
+  }
 
   ngOnInit(): void {
-    console.log("AppInit");
+    console.log("appOnInit");
   }
 
   ngDoCheck(): void {
-    console.log("AppDpoCheck");
-  }
-  
-  @HostListener('window:beforeunload', ['$event'])
-
-  displayContent(e: Event) {
-    let target = e.target as HTMLLinkElement;
-    this.Endpoint = Location.C1;
-    this.Endpoint = this.Endpoint + '' + target.getAttribute('href');
-    console.log(this.Endpoint);
+    console.log("appDoCheck");
   }
 
-  unloadNotification($event: any): void {
-    // Aquí puedes ejecutar acciones antes de que ocurra la recarga de la página.
-    // Por ejemplo, mostrar un mensaje de confirmación.
-    $event.returnValue = true; // Esto muestra un mensaje de confirmación al usuario.
+  ngOnDestroy(): void {
+    console.log("appOnDestroy");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ChildComponent } from './child/child.component';
-import { ContentoneComponent } from './child/contentone/contentone.component';
-import { ContenttwoComponent } from './child/contenttwo/contenttwo.component';
-import { ContentthreeComponent } from './child/contentthree/contentthree.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChildComponent,
-    ContentoneComponent,
-    ContenttwoComponent,
-    ContentthreeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
