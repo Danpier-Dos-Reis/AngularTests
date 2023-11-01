d4e306c68977dd72aa1bfcb0e34d11110bcbc110
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-11-01 12:13:15 -0300

    show and animated cursor in a big square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, OnInit, OnChanges, OnDestroy, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
 import { Image } from 'src/shared/models/Image';
 
 @Component({
@@ -8,29 +8,49 @@ import { Image } from 'src/shared/models/Image';
 })
 export class AppComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
 
-  cssClass:boolean = false;
-  displayParagraph(){
-    this.cssClass = !this.cssClass;
-  }
+  cursorAnimation(e:MouseEvent){
+    const cursorDot:HTMLDivElement = document.querySelector('.cursor-dot')as HTMLDivElement;
+    const cursorOutline:HTMLDivElement = document.querySelector('.cursor-outline') as HTMLDivElement;
+    
+    const posX = e.clientX;
+    const posY = e.clientY;
 
-  hideParagraph(){
-    this.cssClass = !this.cssClass;
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    /*if we uncomment this the out cursor circle will move equal to
+    the pinter*/
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    /*
+    The out cursor circle will move after the pointer
+    */
+    cursorOutline.animate(
+      {
+      left: `${posX}px`,
+      top: `${posY}px`
+      },
+      {
+        duration: 500, fill: 'forwards'
+      }
+    );
   }
 
   //Component LifeCycle
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("appOnChange");
+    // console.log("appOnChange");
   }
 
   ngOnInit(): void {
-    console.log("appOnInit");
+    // console.log("appOnInit");
   }
 
   ngDoCheck(): void {
-    console.log("appDoCheck");
+    // console.log("appDoCheck");
   }
 
   ngOnDestroy(): void {
-    console.log("appOnDestroy");
+    // console.log("appOnDestroy");
   }
 }
